d396b33941acf93dfb035d94c920ff1b363ccc56
Daniel <hdanielmm@gmail.com>
2020-06-18 03:20:39 -0500

    Add some hacks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Charts } from './uncontrolledFormInputs/Charts';
 import UncontrolledFormInput from './uncontrolledFormInputs/UncontrolledFormInput';
 import Emoji from './Emoji';
 import ControlledFormInputs from './controlledFormInputs/ControlledFormInputs';
 
 function App() {
+  const [nameText, setNameText] = useState("")
+  const [emailText, setEmailText] = useState("")
 
   const send = (data) => {
     console.log(data)
+  }
+
+  const handleUpdate = (name, text) => {
+
+    name === "name"
+      ? setNameText(text)
+      : setEmailText(text)
   }
 
   return (
@@ -18,7 +27,18 @@ function App() {
       <UncontrolledFormInput 
         onSend={send}
       />
-      <ControlledFormInputs />
+      <ControlledFormInputs 
+        name="name"
+        placeholder="Name"
+        onChange={handleUpdate}
+      />
+      <ControlledFormInputs 
+        name="email"
+        placeholder="Email"
+        onChange={handleUpdate}
+      />
+      <h4>Nombre: {nameText}</h4>
+      <h4>Email: {emailText}</h4>
     </div>
   );
 }
--- src/controlledFormInputs/ControlledFormInputs.js
+++ src/controlledFormInputs/ControlledFormInputs.js
@@ -1,54 +1,22 @@
 import React, { useState } from 'react'
 
-const ControlledFormInputs = () => {
-  const [name2, setName] = useState("")
-  const [email2, setEmail] = useState("")
-  const [color2, setColor] = useState("#e8e8e8")
+const ControlledFormInputs = ({name, placeholder, onChange }) => {
+  const [text, setText] = useState("")
 
-  const handleChange = (event) => {
-    const {name, value} = event.target
-    let color = "green"
-
-    name === "name" ? setName(value) : setEmail(value)
+  const handleUpdate = (event) => {
+    setText(event.target.value)
     
-    if (name2.trim() === "") color = " #e8e8e8"
-    if (name2.trim() !== "" && name2.trim().length < 5) color = "red"
-
-    setColor(color)
-  }
-
-  const handleClick = () => {
-    console.log("Name:", name2)
-    console.log('Email:', email2)
-  }
-
-  const styles = {
-    border: `1px solid ${color2}`
+    onChange(name, text)
   }
 
   return (
     <div>
-      <h3>Controlled form inputs</h3>
       <input
         type="text"
-        name="name"
-        value={name2}
-        placeholder="Name"
-        onChange={handleChange}
-        style={styles}
+        value={text}
+        placeholder={placeholder}
+        onChange={handleUpdate}
       />
-      <input
-        type="text"
-        name="email"
-        value={email2}
-        placeholder="Email"
-        onChange={handleChange}
-      />
-      <button
-        onClick={handleClick}
-      >
-        Send
-      </button>
     </div>
   )
 }
